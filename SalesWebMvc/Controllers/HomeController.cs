e716a817214ac4104b62db1538b0c6d5dacf2f6f
Lucas Gabriel Chiaretti <lucaschiaretti@hotmail.com>
2020-04-30 22:56:57 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Vendas Web MVC App from C#";
+            ViewData["Desenvolvedor"] = "Lucas Gabriel Chiaretti";
 
             return View();
         }
